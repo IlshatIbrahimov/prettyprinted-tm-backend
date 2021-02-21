2e193bca03f1e687d7135445c25a7c25308ff7c9
Ainur Gimadeev <inuur@mail.ru>
2021-02-21 16:52:23 +0300

    Jpa to Crud repository

--- src/main/java/kfu/group/dev/taskmanager/repository/ProjectRepo.java
+++ src/main/java/kfu/group/dev/taskmanager/repository/ProjectRepo.java
@@ -1,11 +1,11 @@
 package kfu.group.dev.taskmanager.repository;
 
 import kfu.group.dev.taskmanager.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepo extends JpaRepository<Project, Long> {
+public interface ProjectRepo extends CrudRepository<Project, Long> {
 
     Project findById(long id);
 }
--- src/main/java/kfu/group/dev/taskmanager/repository/UserRepo.java
+++ src/main/java/kfu/group/dev/taskmanager/repository/UserRepo.java
@@ -1,11 +1,11 @@
 package kfu.group.dev.taskmanager.repository;
 
 import kfu.group.dev.taskmanager.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends CrudRepository<User, Long> {
 
     User findByEmail(String email);
 }
