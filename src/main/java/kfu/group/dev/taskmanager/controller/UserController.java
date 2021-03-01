898e6d989caf6bf7649c698ae14e18053e176b29
Айнур Гимадеев <inuur@mail.ru>
2021-03-01 18:46:03 +0300

    HotFix - Endpoint to get a user details

--- src/main/java/kfu/group/dev/taskmanager/controller/UserController.java
+++ src/main/java/kfu/group/dev/taskmanager/controller/UserController.java
@@ -1,11 +1,15 @@
 package kfu.group.dev.taskmanager.controller;
 
 import kfu.group.dev.taskmanager.model.User;
+import kfu.group.dev.taskmanager.security.service.CustomUserDetails;
 import kfu.group.dev.taskmanager.service.UserService;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,10 @@ public class UserController {
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @GetMapping("/current")
+    public User getCurrentUser(Authentication authentication) {
+        return userService.getUser(authentication);
     }
 }
