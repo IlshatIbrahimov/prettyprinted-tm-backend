2907e721f9ca9bf314e581c7aab0251291aecabe
Ainur Gimadeev <inuur@mail.ru>
2021-03-28 18:25:36 +0300

    Old assignee event added

--- src/main/java/kfu/group/dev/taskmanager/model/listener/TaskListener.java
+++ src/main/java/kfu/group/dev/taskmanager/model/listener/TaskListener.java
@@ -12,36 +12,23 @@ public class TaskListener {
 
     private static WebSocketService webSocketService;
 
-    @Transient
-    private transient Task beforeChangeTask;
-
     @Autowired
     public void setWebSocketService(WebSocketService webSocketService) {
         TaskListener.webSocketService = webSocketService;
     }
 
-    @PrePersist
-    @PreRemove
-    @PreUpdate
-    public void prePersistOrUpdateOrDelete(Task task) {
-        this.beforeChangeTask = task;
-    }
-
     @PostRemove
     public void postRemove(Task task) {
         webSocketService.taskDeleted(task);
-        webSocketService.taskUpdated(beforeChangeTask);
     }
 
     @PostUpdate
     public void postUpdate(Task task) {
         webSocketService.taskUpdated(task);
-        webSocketService.taskUpdated(beforeChangeTask);
     }
 
     @PostPersist
     public void postPersist(Task task) {
         webSocketService.taskCreated(task);
-        webSocketService.taskUpdated(beforeChangeTask);
     }
 }
