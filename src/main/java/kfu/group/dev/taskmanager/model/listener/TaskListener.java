e2c345acf2c4d5c336cd8e50c00c5b4878430dc9
Ainur Gimadeev <inuur@mail.ru>
2021-03-28 17:59:48 +0300

    WebSocket on project or task comment update handler was added

--- src/main/java/kfu/group/dev/taskmanager/model/listener/TaskListener.java
+++ src/main/java/kfu/group/dev/taskmanager/model/listener/TaskListener.java
@@ -5,32 +5,43 @@ import kfu.group.dev.taskmanager.service.WebSocketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.PostPersist;
-import javax.persistence.PostRemove;
-import javax.persistence.PostUpdate;
+import javax.persistence.*;
 
 @Component
 public class TaskListener {
 
     private static WebSocketService webSocketService;
 
+    @Transient
+    private transient Task beforeChangeTask;
+
     @Autowired
     public void setWebSocketService(WebSocketService webSocketService) {
         TaskListener.webSocketService = webSocketService;
     }
 
+    @PrePersist
+    @PreRemove
+    @PreUpdate
+    public void prePersistOrUpdateOrDelete(Task task) {
+        this.beforeChangeTask = task;
+    }
+
     @PostRemove
     public void postRemove(Task task) {
         webSocketService.taskDeleted(task);
+        webSocketService.taskUpdated(beforeChangeTask);
     }
 
     @PostUpdate
     public void postUpdate(Task task) {
         webSocketService.taskUpdated(task);
+        webSocketService.taskUpdated(beforeChangeTask);
     }
 
     @PostPersist
     public void postPersist(Task task) {
         webSocketService.taskCreated(task);
+        webSocketService.taskUpdated(beforeChangeTask);
     }
 }
